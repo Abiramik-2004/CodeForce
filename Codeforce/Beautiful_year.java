d87e39da5030d857ea20ab43a37de7cb4898ef0d
Abirami k <abiramikanthasamy@gmail.com>
2026-08-25 15:27:10 +0530

    Updated

---
+++ Codeforce/Beautiful_year.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Beautiful_year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+        y++;
+
+        while (true) {
+            String s = String.valueOf(y);
+            boolean distinct = true;
+
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        distinct = false;
+                        break;
+                    }
+                }
+
+                if (!distinct) {
+                    break;
+                }
+            }
+
+            if (distinct) {
+                System.out.println(y);
+                break;
+            }
+
+            y++;
+        }
+    }
+}
