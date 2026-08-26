1a758047d775a3f813395ce4ad0d859f027d541f
Abirami k <abiramikanthasamy@gmail.com>
2026-08-26 14:49:48 +0530

    Updated

---
+++ Codeforce/Soldier_And_Bannanas.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Soldier_And_Bannanas {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        int n=sc.nextInt();
+        int w=sc.nextInt();
+        int tot=k;
+        for(int i=2;i<=w;i++){
+            tot+=k*i;
+        }
+        if(tot>n){
+            System.out.println(tot-n);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+
+}
