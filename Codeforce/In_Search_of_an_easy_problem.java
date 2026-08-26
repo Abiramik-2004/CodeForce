7c882f5624b9d4473cdb8b3c95cce327bcf506b1
Abirami k <abiramikanthasamy@gmail.com>
2026-08-26 15:54:20 +0530

    Updated

---
+++ Codeforce/In_Search_of_an_easy_problem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class In_Search_of_an_easy_problem {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s="Easy";
+        for(int i=0;i<n;i++){
+            int a=sc.nextInt();
+            if(a==1){
+                s="Hard";
+            }
+            
+
+        }
+        System.out.println(s);
+    }
+       
+}
