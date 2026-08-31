3c6639cc1f5004f20a0b0b106a4eeb5b9c31a51c
Abirami k <abiramikanthasamy@gmail.com>
2026-08-31 17:48:12 +0530

    Updated

--- Codeforce/Queue_At_School.java
+++ Codeforce/Queue_At_School.java
@@ -1,7 +1,17 @@
-
+import java.util.*;
 public class Queue_At_School {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the Particular amount");
+            long m=sc.nextLong();
+            System.out.println("Enter the payment mode");
+            System.out.println("1. Cash on delivery\n 2. Pay on google pay\n 3. Pay on paytm\n 4. Pay Later");
+            System.out.println("Enter the option");
+            int option =sc.nextInt();
+            
+        }
 
     }
 }
