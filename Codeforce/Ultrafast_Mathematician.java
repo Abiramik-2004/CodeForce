dc6f3a41f9b9e662f00717046963e27b3456e937
Abirami k <abiramikanthasamy@gmail.com>
2026-08-31 15:39:32 +0530

    Updated

---
+++ Codeforce/Ultrafast_Mathematician.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Ultrafast_Mathematician{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        int i=s1.length()-1;
+        int j=s2.length()-1;
+        String s="";
+        while(i>=0&& j>=0){
+            if(s1.charAt(i)==s2.charAt(j)){
+                    s="0"+s;
+                }
+                else{
+                    s="1"+s;
+                }
+                j--;
+            i--;
+        }
+        System.out.println(s);
+    }
+}
