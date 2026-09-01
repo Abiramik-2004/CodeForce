63b7810147918fb2265db57026194ede988a9bc1
Abirami k <abiramikanthasamy@gmail.com>
2026-09-01 15:06:01 +0530

    Updated

---
+++ Codeforce/horseshoeOnTheHoof.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class horseshoeOnTheHoof {
+
+    public static void main(String ars[]){
+        Scanner sc=new Scanner(System.in);
+        Set<Integer> set=new HashSet();
+        for(int i=0;i<4;i++){
+            int n=sc.nextInt();
+            set.add(n);
+
+        }
+        int count=4-set.size();
+        System.out.println(count);
+        sc.close();
+    }
+}
