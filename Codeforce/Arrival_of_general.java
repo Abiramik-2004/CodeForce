5828d833c3bdfad36d200eee6f9b19e984ac4c38
Abirami k <abiramikanthasamy@gmail.com>
2026-09-02 11:28:55 +0530

    Updated

---
+++ Codeforce/Arrival_of_general.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Arrival_of_general {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int min=0,max=a.length-1, i=1,j=a.length-2;
+        
+        sc.close();
+    }
+}
