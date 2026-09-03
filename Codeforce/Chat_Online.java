eea8322b816db1a21efd17cd8a8a5065a4c6266a
Abirami k <abiramikanthasamy@gmail.com>
2026-09-03 14:58:54 +0530

    Updated

---
+++ Codeforce/Chat_Online.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Chat_Online {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        int[][] z=new int[p][2];
+        int[][] x=new int[q][2];
+        for (int i = 0; i < p; i++) {
+            z[i][0] = sc.nextInt();
+            z[i][1] = sc.nextInt();
+        }
+        for (int i = 0; i < q; i++) {
+            x[i][0] = sc.nextInt();
+            x[i][1] = sc.nextInt();
+        }
+        int count = 0;
+        for (int t = l; t <= r; t++) {
+            boolean canChat = false;
+            for (int i = 0; i < p; i++) {
+                for (int j = 0; j < q; j++) {
+                    int xStart = x[j][0] + t;
+                    int xEnd = x[j][1] + t;
+                    int zStart = z[i][0];
+                    int zEnd = z[i][1];
+                    if (Math.max(xStart, zStart) <= Math.min(xEnd, zEnd)) {
+                        canChat = true;
+                        break;
+                    }
+                }
+                if (canChat) {
+                    break;
+                }
+            }
+            if (canChat) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
