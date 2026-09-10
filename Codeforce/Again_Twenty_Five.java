15a76e00f36aacd8cad7e6b048c7583f3e883eed
Abirami k <abiramikanthasamy@gmail.com>
2026-09-10 11:42:22 +0530

    Updated

--- Codeforce/Again_Twenty_Five.java
+++ Codeforce/Again_Twenty_Five.java
@@ -1,20 +1,12 @@
 import java.util.*;
+
 public class Again_Twenty_Five {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        double n=sc.nextDouble();
-        double s=5;
-        double res=Math.pow(s,n);
-        int count=0;
-        String t="";
-        while(count!=2){
-            int q=(int)res%10;
-            res=res/10;
-            t=q+t;
-            count++;
 
-        }
-        int num=Integer.parseInt(t);
-        System.out.println(num);
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(25);
     }
 }
