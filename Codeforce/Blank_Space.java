2915363b594c47bb69920fde414cf66220b5414a
Abirami k <abiramikanthasamy@gmail.com>
2026-09-22 12:52:38 +0530

    Updated

---
+++ Codeforce/Blank_Space.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Blank_Space{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int l=sc.nextInt();
+            int count=0,maxcount=0;
+            for(int j=0;j<l;j++){
+                int k=sc.nextInt();
+                if(k==0){
+                    count++;
+                }
+                else{
+                    count=0;
+                }
+            }
+            maxcount=Math.max(count, maxcount);
+            System.out.println(maxcount);
+        }
+    }
+}
