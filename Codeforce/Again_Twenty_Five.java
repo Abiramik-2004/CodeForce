35aa6b589e9251b01d8fcb3454e483df00427d8d
Abirami k <abiramikanthasamy@gmail.com>
2026-09-22 14:23:04 +0530

    Updated

--- Codeforce/Again_Twenty_Five.java
+++ Codeforce/Again_Twenty_Five.java
@@ -5,7 +5,7 @@ public class Again_Twenty_Five {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         System.out.println(25);
-        n++;
+        System.out.println(n);
         sc.close();
     }
 }
