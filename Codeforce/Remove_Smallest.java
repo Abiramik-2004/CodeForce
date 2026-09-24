5daf215283d18eda8384598c2c39151a024859d1
Abirami k <abiramikanthasamy@gmail.com>
2026-09-24 11:18:49 +0530

    Updated

--- Codeforce/Remove_Smallest.java
+++ Codeforce/Remove_Smallest.java
@@ -1,25 +1,41 @@
-
 import java.util.*;
 
 public class Remove_Smallest {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
+
         while (t-- > 0) {
+
             int n = sc.nextInt();
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
+
+            int[] a = new int[n];
+
             for (int i = 0; i < n; i++) {
-                int value = sc.nextInt();
-                min = Math.min(min, value);
-                max = Math.max(max, value);
+                a[i] = sc.nextInt();
             }
-            if (max - min <= 1) {
+
+            Arrays.sort(a);
+
+            boolean possible = true;
+
+            for (int i = 1; i < n; i++) {
+
+                if (a[i] - a[i - 1] > 1) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
             }
         }
+
         sc.close();
     }
 }
