0644da56993addc22e7d71d03336abb74d987969
Abirami k <abiramikanthasamy@gmail.com>
2026-09-25 12:05:36 +0530

    Updated

--- Codeforce/Medium_Number.java
+++ Codeforce/Medium_Number.java
@@ -1,22 +1,25 @@
+
 import java.util.*;
+
 public class Medium_Number {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        while(n-->0){
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
-            if(a<b && b<c){
-                System.out.println(b);
-            }
-            else if(b<a && a<c){
-                System.out.println(a);
-            }
-            else if(b<c && a>c){
-                System.out.println(c);
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            int[] arr = {a, b, c};
+
+            Arrays.sort(arr);
+
+            System.out.println(arr[1]);
         }
+
+        sc.close();
     }
-    
 }
+
